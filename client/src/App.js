481124553bfaa38f0345cb0497705181b46d9da9
Cali93 <c.armut93@outlook.com>
2018-02-23 23:16:31 +0100

    react App.js ready to go

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,24 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { Router, browserHistory } from 'react-router';
+import PropTypes from 'prop-types';
+import { syncHistoryWithStore } from 'react-router-redux';
+import configureStore from './store/configureStore';
 
+class App extends Component {
+  render(){
+    return 
+      <Provider store={store}>
+        <div>
+          <Router history={history} routes={routes}/>
+        </div>
+      </Provider>
+  }
+}
+
+App.PropTypes = {
+  store: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired
+}
+
+export default App;
