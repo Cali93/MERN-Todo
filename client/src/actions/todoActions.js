df49def4d6df62c71fa075ee299f1436712b0356
Cali93 <c.armut93@outlook.com>
2018-02-25 12:30:10 +0100

    actions added

---
+++ client/src/actions/todoActions.js
@@ -0,0 +1,109 @@
+const apiUrl = "/api/todos/";
+
+export const toggleAddBook = () => {
+  return {
+    type: 'TOGGLE_ADD_TODO'
+  }
+}
+
+export const newTodo = (todo) => {
+
+}
+
+export const deleteTodo = (todo) => {
+
+}
+
+export const editTodo = (todo) => {
+
+}
+
+// Async action
+
+export const fetchTodos = () => {
+  // Returns a dispatcher function that dispatches an action at a later time
+  return (dispatch) => {
+    dispatch(fetchTodosRequest());
+    // Returs a promise
+    return fetch(apiUrl)
+              .then(response => {
+                if (response.ok){
+                  response.json().then(data => {
+                    dispatch(fetchTodosSuccess(data.todos, data.message));
+                  })
+                } else {
+                  response.json().then(error => {
+                    dispatch(fetchTodosFailed(error));
+                  })
+                }
+              })
+  }
+}
+
+export const fetchTodosRequest = () => {
+  return {
+    type: 'FETCH_TODOS_REQUEST'
+  }
+}
+
+// Sync action
+export const fetchTodosSuccess = (todos, message) => {
+  return {
+    type: 'FETCH_TODOS_SUCCESS',
+    todos: todos,
+    message: message,
+    receivedAt: Date.now
+  }
+}
+
+export const fetchTodosFailed = (error) => {
+  return {
+    type: 'FETCH_TODOS_FAILED',
+    error
+  }
+}
+
+export const fetchTodoById = (todoId) => {
+  // Returns a anonymous function with dispatch in param
+  return (dispatch) => {
+    dispatch(fetchTodoRequest());
+    // Returs a promise
+    return fetch(apiUrl + todoId)
+              .then(response => {
+                console.log(response)
+                if(response.ok){
+                  res.json().then(data => {
+                    dispatch(fetchTodoSuccess(dtata.todo[0],data.message));
+                  })
+                } else {
+                  res.json().then(error => {
+                    dispatch(fetchTodoFailed(error));
+                  })
+                }
+              })
+  }
+}
+
+export const fetchTodoRequest = () => {
+  return {
+    type: 'FETCH_TODO_REQUEST'
+  }
+}
+
+// Sync action
+
+export const fetchTodoSuccess = (todo,messsage) => {
+  return {
+    type: 'FETCH_TODO_SUCCESS',
+    todo: todo,
+    message: message,
+    receivedAt: Date.now
+  }
+}
+
+export const fetchTodoFailed = (error) => {
+  return {
+    type: 'FETCH_TODO_FAILED',
+    error
+  }
+}
